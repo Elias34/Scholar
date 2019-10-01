a1ab5c60923e8b26b265eab70897481e8ea7904b
justyn <justinmesa2013@gmail.com>
2019-10-01 08:06:00 -0500

    Marcos Caro

---
+++ Arreglos.java
@@ -0,0 +1,28 @@
+
+package arreglos;
+
+
+public class Arreglos {
+
+    
+    public static void main(String[] args) {
+       String[] nombres ={"Jaime", "Mariam" , "Michel", "Paola", "2001"};
+       
+       for(String i:nombres){
+             System.out.println("Nombres:" +i);
+           
+           
+           
+           
+           
+           
+       }
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ Elias.java
@@ -0,0 +1,32 @@
+
+package elias;
+
+import java.util.Scanner;
+
+
+public class Elias {
+
+   
+    public static void main(String[] args) {
+      
+         int numeros[]= new int[5];
+        
+        for (int i=0; i<6; i++){
+         Scanner lectura= new Scanner(System.in);
+         System.out.println("Digite numero");
+         numeros[i]=lectura.nextInt();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
